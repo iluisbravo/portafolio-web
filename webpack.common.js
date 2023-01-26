a4f97f93155398b313abbd4930e7c26d3eb3dfd0
iluisbravo <ing.LuisAlbertoBravo@gmail.com>
2023-01-26 09:04:46 -0700

    arreglando dependencias

--- webpack.common.js
+++ webpack.common.js
@@ -9,19 +9,6 @@ module.exports = {
       {
         test: /\.html$/,
         use: ["html-loader"]
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|svg|pdf)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[hash].[ext]",
-              outputPath: "assets",
-              esModule: false
-            }
-          }
-        ]
       }
     ]
   },
